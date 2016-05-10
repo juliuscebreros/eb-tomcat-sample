81c6b1b40ca59fa8ea38eccd8b499fb05d4c9be6
Julius <julius@cloud.house>
2016-05-10 01:33:16 +0000

    Use environment config

---
+++ create_env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+aws --profile ics-qa elasticbeanstalk create-environment \
+  --application-name ICS-API \
+  --environment-name ICS-DEV \
+  --solution-stack-name "64bit Amazon Linux 2016.03 v2.1.1 running Tomcat 8 Java 8"
+  --version-label v3
