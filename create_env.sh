eae449628112afefb258b14174d5437345ce777d
Julius <julius@cloud.house>
2016-05-10 02:05:25 +0000

    Test changing a parameter

--- create_env.sh
+++ create_env.sh
@@ -2,6 +2,6 @@
 
 aws --profile ics-qa elasticbeanstalk create-environment \
   --application-name ICS-API \
-  --environment-name ICS-DEV-2 \
+  --environment-name ICS-DEV-3 \
   --solution-stack-name "64bit Amazon Linux 2016.03 v2.1.1 running Tomcat 8 Java 8" \
-  --version-label v4
+  --version-label v5
